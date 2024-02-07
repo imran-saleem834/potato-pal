57ec40a582d32ada1aa92ca8c31f4b54c8e1b030
Imran Saleem <imran.sheikh834@gmail.com>
2024-02-07 14:59:47 +0500

    removed buttons from home page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,18 +2,21 @@
 import { Head, Link } from '@inertiajs/vue3';
 import LogoutButton from '@/Components/LogoutButton.vue';
 
+// route('reallocations.index')
+// route('dispatches.index')
+// route('cuttings.index')
 const menus = [
   { route: route('users.index'), image: 'menu1.png', label: 'Users' },
   { route: route('receivals.index'), image: 'menu2.png', label: 'Receivals' },
   { route: route('unloading.index'), image: 'menu3.png', label: 'Unloading' },
   { route: route('tia-samples.index'), image: 'menu4.png', label: 'TIA Sampling' },
   { route: route('allocations.index'), image: 'menu5.png', label: 'Allocations' },
-  { route: route('reallocations.index'), image: 'menu6.png', label: 'Reallocations' },
+  { route: '', image: 'menu6.png', label: 'Reallocations' },
   { route: '', image: 'menu7.png', label: 'Invoices' },
   { route: '', image: 'menu8.png', label: 'Labels' },
-  { route: route('dispatches.index'), image: 'menu9.png', label: 'Dispatch' },
+  { route: '', image: 'menu9.png', label: 'Dispatch' },
   { route: '', image: 'menu10.png', label: 'Reports' },
-  { route: route('cuttings.index'), image: 'menu11.png', label: 'Cutting' },
+  { route: '', image: 'menu11.png', label: 'Cutting' },
   { route: '', image: 'menu12.png', label: 'Weighbridge' },
   { route: route('categories.index'), image: 'menu13.png', label: 'Admin Options' },
   { route: route('notifications.index'), image: 'menu14.png', label: 'Notifications' },
